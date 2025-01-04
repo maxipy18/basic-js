63cf0622fbb55a6acaf5aefa7f382ec17ea7a50f
Maxim Krestyaninov <maxipy@ya.ru>
2025-01-04 20:46:24 +0400

    feat: createDreamTeam

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,14 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
+  const result = [];
+  const nameArray = members.filter((name) => typeof(name) === 'string'); 
+  nameArray.forEach((name) => {
+    result.push(name.trim().toUpperCase()[0])
+  });
+  return result.sort().join('');
 }
 
 module.exports = {
