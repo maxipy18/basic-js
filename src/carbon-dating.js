a49e6570c75b9cd832e6991682699ebf86a62a2e
Maxim Krestyaninov <maxipy@ya.ru>
2025-01-05 19:29:47 +0400

    feat: add dateSample

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,18 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof(sampleActivity) === 'string') {
+    const num = Number(sampleActivity)
+    if (num > 0 && num < MODERN_ACTIVITY){
+      if (!isNaN(num)) {
+        const k = 0.693 / HALF_LIFE_PERIOD;
+        const ln = Math.log(MODERN_ACTIVITY / num)
+        return Math.ceil(ln/k);
+      }
+    }    
+  }
+  return false;
 }
 
 module.exports = {
